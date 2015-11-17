a213fea2e6cedaabbd96831ffb495b4db6dfcf8c
ctevans <cevans2@ualberta.ca>
2015-11-16 19:43:29 -0700

    fixed a few bugs, mostly functional part 1

--- p2.py
+++ p2.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import re
+
 #Open the file and only have it so that it is being read.
 f = open('10.txt', "r")
 o = open('reviews.txt', "w")
@@ -7,13 +10,12 @@ o = open('reviews.txt', "w")
 line  = True
 counter = 1
 counterChange = True
+noCommaFlag = False
 
 
 
-
-print(counter,end="")
 o.write(str(counter))
-#print(",",end="") # That initial 1 
+o.write(",")
 #series of while loops that parse the input a metric ton.
 while line:
     line = f.readline()
@@ -34,15 +36,49 @@ while line:
 
     if line == "\n":
         print()
+        o.write("\n")
         counter = counter +1
         print(counter,end="")
         o.write(str(counter))
+        noCommaFlag = True
+
+
+    if (splittingFunction[0] == "review/text"):
+        continue
 
     if (splittingFunction[0] == "review/text") or (splittingFunction[0] == "product/title") or(splittingFunction[0] == "review/summary") or (splittingFunction[0] == "review/profileName"):
         print("\"",end="")
         o.write("\"")
     print(",", end="")
+
+    #if (noCommaFlag != True):
     o.write(",")
+    #    noCommaFlag = False
+    #noCommaFlag = False
         
 
 f.close()
+
+
+#Part where I do all of the counting of all of the various words.
+f2 = open('10.txt', "r")
+
+numOccurances = {}
+
+fileContents2 = f2.read()
+file2Words = fileContents2.split(' ') #split by spaces
+for word in file2Words:
+    if word not in numOccurances:
+        numOccurances[word] = 1
+    else: 
+        numOccurances[word] += 1
+
+for (key,value) in numOccurances:
+    print(key, value)
+
+
+
+
+
+
+
