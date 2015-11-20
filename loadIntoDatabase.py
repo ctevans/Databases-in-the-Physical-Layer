1913f8ac87e44134fd72655c6cfc22b8d195f39e
ctevans <cevans2@ualberta.ca>
2015-11-20 15:32:17 -0700

    added script files

--- loadIntoDatabase.py
+++ loadIntoDatabase.py
@@ -1,4 +1,5 @@
 from bsddb3 import db 
 #Get an instance of BerkeleyDB 
 database = db.DB() 
-database.open("mydatabase", None, db.DB_HASH, db.DB_CREATE)
+database.open("sc.idx", None, db.DB_BTREE, db.DB_CREATE)
+database.close()
