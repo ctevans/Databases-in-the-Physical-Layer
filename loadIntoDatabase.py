64127a4d135629d247ad57a6fd96b94b07f0af58
ctevans <cevans2@ualberta.ca>
2015-11-20 18:49:44 -0700

    another commit again. lots of changes

--- loadIntoDatabase.py
+++ loadIntoDatabase.py
@@ -1,5 +1,16 @@
 from bsddb3 import db 
 #Get an instance of BerkeleyDB 
 database = db.DB() 
-database.open("sc.idx", None, db.DB_BTREE, db.DB_CREATE)
+database.set_flags(db.DB_DUP)
+database.open("sc.idx", None, db.DB_HASH, db.DB_CREATE)
+cur = database.cursor()
+
+cur.put(b'blue', "berry", db.DB_KEYFIRST)
+cur.put(b'blue', "lemon",db.DB_KEYFIRST)
+cur.first()
+
+print(cur.count())
+print(cur.next_dup())
+
+
 database.close()
