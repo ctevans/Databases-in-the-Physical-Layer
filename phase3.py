c39aa6e64392e18d34a6b8e62bb42237bff53224
Si He <syhe@cs.ualberta.ca>
2015-11-22 02:43:15 -0700

    getting crazy errors, couldn't find a fix, calling it a night.

--- phase3.py
+++ phase3.py
@@ -8,7 +8,7 @@ def main():
 		qList = []
 		database = db.DB() 
 		reviewDB = db.DB()
-		reviewDB.open("rw.idx")
+		reviewDB.open("rw.idx", None, db.DB_HASH, db.DB_CREATE)
 
 		user_input = input("Enter your queries here: ")
 		if user_input.find(":") != -1:
@@ -24,11 +24,11 @@ def main():
 
 
 		if Dname == "p":
-			database.open("pt.idx")
+			database.open("pt.idx", None, db.DB_HASH, db.DB_CREATE)
 		elif Dname == "r":
-			database.open("rt.idx")
+			database.open("rt.idx", None, db.DB_HASH, db.DB_CREATE)
 		elif Dname == "rs":
-			database.open("sc.idx")
+			database.open("sc.idx", None, db.DB_HASH, db.DB_CREATE)
 
 		cur = database.cursor()
 		rcur = reviewDB.cursor()
@@ -41,17 +41,18 @@ def main():
 						if iter[1].decode("utf-8") not in qList:
 							qList.append(iter[1].decode("utf-8"))
 					iter = cur.next()
-			elif Dname == "" :
-				iter = rcur.first()
-				while iter:
-					if i in iter[1].decode("utf-8"):
-						print(iter[0].decode("utf-8"))
-					# if iter[0].decode("utf-8") in qList:
+			# elif Dname == "" :
+			# 	iter = rcur.first()
+			# 	while iter:
+			# 		if i in iter[1].decode("utf-8"):
+			# 			print(iter[0].decode("utf-8"))
+			# 		# if iter[0].decode("utf-8") in qList:
 
 
 		rcur.close()
 		cur.close()
 		print(qList)
+		rcur = reviewDB.cursor()
 		if qList == []:
 			print("Sorry we do not have any data on your queries.")
 		iter = rcur.first()
