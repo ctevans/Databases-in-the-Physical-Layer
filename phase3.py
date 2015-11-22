cc62a40c9a8a3a6c38fec76c712c36c9194b63a8
Si He <syhe@cs.ualberta.ca>
2015-11-21 20:34:40 -0700

    updated part3.

--- phase3.py
+++ phase3.py
@@ -2,21 +2,29 @@ from bsddb3 import db
 #Get an instance of BerkeleyDB
 Runtime = True
 while Runtime:
+	Dname = ""
 	database = db.DB() 
 	user_input = input("Enter your queries here:")
 	if user_input.find(":") != -1:
 		split_input = user_input.split(":")
 		Dname = split_input[0]
 		del split_input[0]
+		split_input = split_input[0].split()
 	else:
 		split_input = user_input.split()
-	print(Dname,split_input)
-	database.open("sc.idx")
+		atabase.open("rw.idx")
+	print(split_input)
+	if Dname :
+		if Dname == "p":
+			database.open("pt.idx")
+		elif Dname == "r":
+			database.open("rt.idx")
 	cur = database.cursor()
 	iter = cur.first()
 	while iter:
-	 print(iter)
-	 iter = cur.next()
+		if iter[0].decode("utf-8") == "art":
+			print(iter)
+		iter = cur.next()
 	cur.close()
 	Termniate = input("Is that all? (T/F) :")
 	if Termniate == "T":
